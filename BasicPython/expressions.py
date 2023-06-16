865e4dd73923d8d69b0ea3272efcaa1b17fdaf87
Pushkar Gupta <pushkargupta@pushkars-MacBook-Pro.local>
2023-06-16 18:24:58 +0530

    variables and expressions

---
+++ BasicPython/expressions.py
@@ -0,0 +1,34 @@
+base = 5
+height = 3
+area = (base*height)/2.
+
+print(area)
+
+total = 2048 + 4357 + 97658 + 125 + 8
+files = 5
+average = total / files
+print("The average size is: " + str(average))
+
+print("2 + 2 = " + str(2 + 2))
+
+bill = 47.28 # Assign "bill" variable with bill amount
+tip = bill * (0.15) # Multiply by stated tip rate 
+total = bill + tip # Sum the "total" of the "bill" and "tip"
+share = total/2 # Divide "total" by number of friends dining
+print("Each person needs to pay: " + str(share)) # Enter the required string and "share" 
+# Hint: Remember to convert incompatible data types
+
+numerator = 10
+denominator = 10
+result = numerator / denominator
+print(result)
+
+word1 = "How"
+word2 = "do"
+word3 = "you"
+word4 = "like"
+word5 = "Python"
+word6 = "so"
+word7 = "far?"
+
+print(word1 + " " + word2 + " " + word3 + " " + word4 + " " + word5 + " " + word6 + " " + word7)
