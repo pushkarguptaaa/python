02092c5373521f62bfccebb436a3e39337bf8882
Pushkar Gupta <pushkargupta@pushkars-MacBook-Pro.local>
2023-06-20 22:20:15 +0530

    for loop

---
+++ Loops/for.py
@@ -0,0 +1,20 @@
+sum = 0
+for n in range(1,5):
+    sum+= n
+print(sum)
+
+for n in range(6,18+1,3):
+    print(n*2)
+
+for n in range(19):
+    if n % 2 == 0:
+        print(n)
+
+for n in range(10):
+    print(n+n)
+
+for x in range(1,10):
+  print(x**3)
+
+for x in range(0,100,7): 
+    print(x)
