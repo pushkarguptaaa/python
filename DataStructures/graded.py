bdaa194934047ba51a55bcb021997b88426e77b9
Pushkar Gupta <pushkargupta@pushkars-MacBook-Pro.local>
2023-06-23 23:33:31 +0530

    graded data structures

---
+++ DataStructures/graded.py
@@ -0,0 +1,80 @@
+def format_address(address_string):
+
+
+    house_number = ""
+    street_name = ""
+
+
+    # Separate the house number from the street name.
+    address_parts = address_string.split()
+    
+    for address_part in address_parts:
+       # Complete the if-statement with a string method.  
+       if address_part.isnumeric():
+         house_number = address_part
+       else:
+         street_name += address_part + " "
+    # Remove the extra space at the end of the last "street_name".
+    street_name = street_name.strip()
+ 
+    # Use a string method to return the required formatted string.
+    return "House number " + house_number + " on a street named " + street_name
+
+
+print(format_address("123 Main Street"))
+# Should print: "House number 123 on a street named Main Street"
+
+
+print(format_address("1001 1st Ave"))
+# Should print: "House number 1001 on a street named 1st Ave"
+
+
+print(format_address("55 North Center Drive"))
+# Should print "House number 55 on a street named North Center Drive"
+
+def highlight_word(sentence, word):
+    # Complete the return statement using a string method.
+    return sentence.replace(word,word.upper())
+
+
+print(highlight_word("Have a nice day", "nice"))
+# Should print: "Have a NICE day"
+print(highlight_word("Shhh, don't be so loud!", "loud"))
+# Should print: "Shhh, don't be so LOUD!"
+print(highlight_word("Automating with Python is fun", "fun"))
+# Should print: "Automating with Python is FUN"
+
+def even_numbers(first, last):
+  return [ n for n in range(first,last) if n%2 == 0 ]
+
+
+print(even_numbers(4, 14)) # Should print [4, 6, 8, 10, 12]
+print(even_numbers(0, 9))  # Should print [0, 2, 4, 6, 8]
+print(even_numbers(2, 7))  # Should print [2, 4, 6]
+
+def countries(countries_dict):
+    result = ""
+    # Complete the for loop to iterate through the key and value items 
+    # in the dictionary.
+    for cont,count in countries_dict.items():
+        # Use a string method to format the required string.
+        result += str(count) + "\n"
+    return result
+
+print(countries({"Africa": ["Kenya", "Egypt", "Nigeria"], "Asia":["China", "India", "Thailand"], "South America": ["Ecuador", "Bolivia", "Brazil"]}))
+
+# Should print:
+# ['Kenya', 'Egypt', 'Nigeria']
+# ['China', 'India', 'Thailand']
+# ['Ecuador', 'Bolivia', 'Brazil']
+
+def check_guests(guest_list, guest):
+  return guest_list[guest] # Return the value for the given key
+
+
+guest_list = { "Adam":3, "Camila":3, "David":5, "Jamal":3, "Charley":2, "Titus":1, "Raj":6, "Noemi":1, "Sakira":3, "Chidi":5}
+
+
+print(check_guests(guest_list, "Adam")) # Should print 3
+print(check_guests(guest_list, "Sakira")) # Should print 3
+print(check_guests(guest_list, "Charley")) # Should print 2
